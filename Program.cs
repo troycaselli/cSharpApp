eb51e78f4b0250c1e092e3c3e648608488748b87
troycaselli <swordandtrowel1689@gmail.com>
2023-12-09 13:54:54 -0800

    added boolean coin flipper

--- Program.cs
+++ Program.cs
@@ -2,6 +2,29 @@
 // dotnet build and/or dotnet run
 
 
+// ======== COIN FLIPPER ========================================================
+
+// Random random = new();
+// Console.WriteLine((random.NextDouble() > 0.5) ? "heads" : "tails");
+
+
+// ========= BOOLEAN EXPRESSIONS =================================================
+
+// string value1 = " a";
+// string value2 = "A";
+// Console.WriteLine(value1.Trim().ToUpper() == value2.Trim().ToUpper());
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("hello"));
+
+// int a = 7;
+// int b = 6;
+// Console.WriteLine(a != b);
+// int discount = 120 > 100 ? 10 : 5;
+// Console.WriteLine($"Discount: {discount}");
+
+
 // ========= REPORTING FRAUDULENT IDS =============================================
 
 // string[] ids = {"B123",
