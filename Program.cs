7640fa6519a5f7e1b952b95b9795d69858291d4e
troycaselli <swordandtrowel1689@gmail.com>
2023-12-14 10:45:32 -0800

    switch statements

--- Program.cs
+++ Program.cs
@@ -1,33 +1,115 @@
 ﻿// terminal:
 // dotnet build and/or dotnet run
 
+// ======== SWITCH STATEMENTS ==================================================
+
+int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
+
+// SKU = Stock Keeping Unit.
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+         type = "Sweat pants";
+         break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color ="Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+Console.WriteLine($"Product: {size} {color} {type}");
 
 // ======== VARIABLE SCOPE ======================================================
 
-bool flag = true;
-int value = 0;
+// bool flag = true;
+// int value = 0;
 
-if (flag)
-{
-    value = 10;
-    Console.WriteLine($"Inside of code block: {value}");
-}
-Console.WriteLine($"Outside of code block: {value}");
+// if (flag)
+// {
+//     value = 10;
+//     Console.WriteLine($"Inside of code block: {value}");
+// }
+// Console.WriteLine($"Outside of code block: {value}");
 
 
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
-int total = 0;
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0;
 
-foreach (int number in numbers)
-{
-    total += number;
+// foreach (int number in numbers)
+// {
+//     total += number;
 
-    if (number == 42)
-    {
-        Console.WriteLine("Set contains 42");
-    }
-}
-Console.WriteLine($"Total: {total}");
+//     if (number == 42)
+//     {
+//         Console.WriteLine("Set contains 42");
+//     }
+// }
+// Console.WriteLine($"Total: {total}");
 
 // ======== COIN FLIPPER ========================================================
 
