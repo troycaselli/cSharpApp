e66cc6f743d47cd0db89b5a1cdc0733a438981f0
troycaselli <swordandtrowel1689@gmail.com>
2023-12-15 09:53:24 -0800

    added for loops and fizzbuzz coding challenge

--- Program.cs
+++ Program.cs
@@ -1,85 +1,112 @@
 ﻿// terminal:
 // dotnet build and/or dotnet run
 
+// ======= FIZBUZZ CODING CHALLENGE ==========================================
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0) Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0) Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0) Console.WriteLine($"{i} - Buzz");
+    else Console.WriteLine(i);
+}
+
+
+// ======== FOR LOOPS =========================================================
+
+// string[] names = {"Alex", "Eddie", "David", "Michael"};
+// for (int i = 0; i < names.Length; i++)
+// {
+//     Console.WriteLine(names[i]);
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+//     if (i == 7) break;
+// }
+
+
 // ======== SWITCH STATEMENTS ==================================================
 
-int employeeLevel = 100;
-string employeeName = "John Smith";
-string title = "";
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
+// string title = "";
 
-switch (employeeLevel)
-{
-    case 100:
-    case 200:
-        title = "Senior Associate";
-        break;
-    case 300:
-        title = "Manager";
-        break;
-    case 400:
-        title = "Senior Manager";
-        break;
-    default:
-        title = "Associate";
-        break;
-}
-Console.WriteLine($"{employeeName}, {title}");
+// switch (employeeLevel)
+// {
+//     case 100:
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
+// Console.WriteLine($"{employeeName}, {title}");
 
 
-// SKU = Stock Keeping Unit.
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-string[] product = sku.Split('-');
+// // SKU = Stock Keeping Unit.
+// // SKU value format: <product #>-<2-letter color code>-<size code>
+// string sku = "01-MN-L";
+// string[] product = sku.Split('-');
 
-string type = "";
-string color = "";
-string size = "";
+// string type = "";
+// string color = "";
+// string size = "";
 
-switch (product[0])
-{
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-         type = "Sweat pants";
-         break;
-    default:
-        type = "Other";
-        break;
-}
+// switch (product[0])
+// {
+//     case "01":
+//         type = "Sweat shirt";
+//         break;
+//     case "02":
+//         type = "T-Shirt";
+//         break;
+//     case "03":
+//          type = "Sweat pants";
+//          break;
+//     default:
+//         type = "Other";
+//         break;
+// }
 
-switch (product[1])
-{
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color ="Maroon";
-        break;
-    default:
-        color = "White";
-        break;
-}
+// switch (product[1])
+// {
+//     case "BL":
+//         color = "Black";
+//         break;
+//     case "MN":
+//         color ="Maroon";
+//         break;
+//     default:
+//         color = "White";
+//         break;
+// }
 
-switch (product[2])
-{
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
-Console.WriteLine($"Product: {size} {color} {type}");
+// switch (product[2])
+// {
+//     case "S":
+//         size = "Small";
+//         break;
+//     case "M":
+//         size = "Medium";
+//         break;
+//     case "L":
+//         size = "Large";
+//         break;
+//     default:
+//         size = "One Size Fits All";
+//         break;
+// }
+// Console.WriteLine($"Product: {size} {color} {type}");
+
 
 // ======= PERMISSIONS LOGIC ===================================================
 
