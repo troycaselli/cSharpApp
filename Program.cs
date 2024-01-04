b54bb18e4e0601b8b63b5daf612bd9d164c3a4ca
troycaselli <swordandtrowel1689@gmail.com>
2024-01-04 09:03:37 -0800

    array sort and reverse

--- Program.cs
+++ Program.cs
@@ -1,21 +1,40 @@
 ﻿// terminal:
 // dotnet build and/or dotnet run
 
+// ====== ARRAYS ================================================================
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted....");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
 // ====== EVALUATE NUMBER TYPES ==================================================
 
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+// int value1 = 12;
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
 
-int result1 = (int)Math.Round(value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// int result1 = (int)Math.Round(value1 / value2);
+// Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
 
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+// decimal result2 = value2 / (decimal)value3;
+// Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+// float result3 = value3 / value1;
+// Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
 
 
 // ====== EVALUATE DATA ===========================================================
