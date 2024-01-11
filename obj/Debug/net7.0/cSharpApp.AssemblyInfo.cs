a15175cbf4373037a00cfc000108266a66a8c657
troycaselli <swordandtrowel1689@gmail.com>
2024-01-11 13:34:13 -0800

    added string challenge

--- obj/Debug/net7.0/cSharpApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/cSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67f4b3f07163271688b3e19e12d7505b61b738c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af427ffc6e88ad0ba4e2f86809785a447052a9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
